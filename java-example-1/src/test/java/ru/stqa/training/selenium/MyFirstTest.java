267d9de266afed0dfe53c4f16d16d9528335cc78
newsyby <newsyby@gmail.com>
2017-03-26 22:01:19 +0300

    Задание 1. Подготовьте инфраструктуру

---
+++ java-example-1/src/test/java/ru/stqa/training/selenium/MyFirstTest.java
@@ -0,0 +1,21 @@
+package ru.stqa.training.selenium;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import java.util.concurrent.TimeUnit;
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class MyFirstTest extends TestBase {
+
+    @Test
+    public void myFirstTest() {
+        driver.navigate().to("http://www.google.com");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.findElement(By.name("q")).sendKeys("http://software-testing.ru/");
+        driver.findElement(By.name("btnG")).click();
+        wait.until(titleIs("http://software-testing.ru/ - Поиск в Google"));
+    }
+
+
+}
